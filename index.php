fc25495bb8b389f2c697884a2fc1e38406ac08c1
enoch finley <enochfinley@gmail.com>
2015-09-04 16:15:09 -0600

    carver messed with the index

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                         require_once 'vendor\autoload.php';
                         use WindowsAzure\Common\ServicesBuilder;          
 
-                        $connectionString='DefaultEndpointsProtocol=https;AccountName=dbt330;AccountKey=ixV/NG4/Jy8P9Pk3pByAE3hD8cTfgD7Mb0Anredi0rktSd/NZddt0e63VPfjQW8vCrIc4vapgHeJpSh3ALkaudQ==';
+                        $connectionString='DefaultEndpointsProtocol=https;AccountName=dbt330;AccountKey=ixV/NG4/Jy8P9Pk3pByAE3hD8cTfgD7Mb0Anredi0rktSd/NZddt0e63VPfjQW8vCrIc4vapgHeJph3ALkaudQ==';
                         $url = $_SERVER['PATH_INFO'];
                         $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
                         $getRekt = (parse_url($url, PHP_URL_PATH));
