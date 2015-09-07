e536c48a7edacc6f949f4f1f025127950e00fa2b
John Anglin <anglicarver@gmail.com>
2015-09-06 18:37:47 -0600

    fixed buttons, finished delete

--- delete.php
+++ delete.php
@@ -1,14 +1,18 @@
 <?php
-                        require_once 'vendor\autoload.php';
-                        use WindowsAzure\Common\ServicesBuilder;          
+    require_once 'vendor\autoload.php';
+    use WindowsAzure\Common\ServicesBuilder;
 
-                        $connectionString='DefaultEndpointsProtocol=https;AccountName=dbt330;AccountKey=ixV/NG4/Jy8P9Pk3pByAE3hD8cTfgD7Mb0Anredi0rktSd/NZddt0e63VPfjQW8vCrIc4vapgHeJph3ALkaudQ==';
-                        $url = $_SERVER['PATH_INFO'];
-                        $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-                        $getRektArg = (parse_url($url, PHP_URL_QUERY));
-                        try{                                              //this getRekt is the string it looks for in the wiki2 collection
-                            $blobRestProxy->deleteBlob("wiki2",explode("=",$getRekt)[1]);
-                        } catch(Exception $e){
-                                echo "<p id='error'><b>Error 404:</b> The specified page was not found to be deleted</p>";
-                        }
+$toDelete = $_GET['toDelete'];
+
+    $connectionString='DefaultEndpointsProtocol=https;AccountName=dbt330;AccountKey=ixV/NG4/Jy8P9Pk3pByAE3hD8cTfgD7Mb0Anredi0rktSd/NZddt0e63VPfjQW8vCrIc4vapgHeJph3ALkaudQ==';
+$url = $_SERVER['PATH_INFO'];
+    $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+
+    try{                                             
+        $blobRestProxy->deleteBlob("wiki2", $toDelete);
+    } catch(Exception $e){
+        echo "<p id='error'><b>Error 404:</b> The specified page was not found to be deleted</p>";
+            echo $e->getMessage();
+    }
 ?>
+
--- script/index.js
+++ script/index.js
@@ -2,8 +2,18 @@
 $(document).ready(function() {
     
     $("#delete").click(function deletArticle(title) {
-        //$.get("delete.php/?arg=" + title);
-        console.log("test");
+        if (confirm("Are you sure you want to delete this page? It cannot be undone")) {
+            console.log("Delete attempt");
+            var pathname = window.location.pathname.split("/");
+            var toDelete = pathname[pathname.length-1];
+            toDelete = toDelete.replace("%20", " ");
+            console.log(toDelete);
+            var val  = $.get("delete.php", {toDelete : toDelete});
+            console.log(val);
+            alert("Page successfully deleted");
+            window.location.replace("A");
+        }
+        
         return false;
     });
     
